4212e51863a649283017c83803202ac308e3e9b7
Icarus <163467778+Icaro-Costa@users.noreply.github.com>
2024-10-08 00:06:45 -0300

    V5

--- js/scrips.js
+++ js/scrips.js
@@ -2,3 +2,7 @@ window.onload = function() {
     document.body.classList.add('no-scroll');
 };
 
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('show');
+}
